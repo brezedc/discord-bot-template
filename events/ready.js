371a80bb21ed8cf0c39d428ac24692eacffa200d
Mezo <dominik@koch-bautechnik.de>
2022-04-28 19:38:47 +0200

    Small improvements

--- events/ready.js
+++ events/ready.js
@@ -1,43 +1,50 @@
-const config = require("../config.json");
+const { devMode, guildid } = require("../config.json");
 
 module.exports = async (client) => {
-    client.user.setPresence({ activities: [{ name: "Status" }], status: "online" });
+  client.user.setPresence({
+    activities: [{ name: "Status" }],
+    status: "online",
+  });
 
-    if (config.devMode) {
-        let guild = client.guilds.cache.get(config.guild);
-        await guild.commands.set(client.rawSlashCommands).then((cmd) => {
-            const getRoles = (commandName) => {
-                const permissions = client.rawSlashCommands.find((x) => x.name === commandName).userPermissions;
+  if (devMode) {
+    let guild = client.guilds.cache.get(guildid);
+    await guild.commands.set(client.rawSlashCommands).then((cmd) => {
+      const getRoles = (commandName) => {
+        const permissions = client.rawSlashCommands.find(
+          (x) => x.name === commandName
+        ).userPermissions;
 
-                if (!permissions) return null;
-                return guild.roles.cache.filter((x) => x.permissions.has(permissions) && !x.managed);
-            };
+        if (!permissions) return null;
+        return guild.roles.cache.filter(
+          (x) => x.permissions.has(permissions) && !x.managed
+        );
+      };
 
-            const fullPermissions = cmd.reduce((accumulator, x) => {
-                const roles = getRoles(x.name);
-                if (!roles) return accumulator;
+      const fullPermissions = cmd.reduce((accumulator, x) => {
+        const roles = getRoles(x.name);
+        if (!roles) return accumulator;
 
-                const permissions = roles.reduce((a, v) => {
-                    return [
-                        ...a,
-                        {
-                            id: v.id,
-                            type: "ROLE",
-                            permission: true,
-                        },
-                    ];
-                }, []);
+        const permissions = roles.reduce((a, v) => {
+          return [
+            ...a,
+            {
+              id: v.id,
+              type: "ROLE",
+              permission: true,
+            },
+          ];
+        }, []);
 
-                return [
-                    ...accumulator,
-                    {
-                        id: x.id,
-                        permissions,
-                    },
-                ];
-            }, []);
+        return [
+          ...accumulator,
+          {
+            id: x.id,
+            permissions,
+          },
+        ];
+      }, []);
 
-            guild.commands.permissions.set({ fullPermissions });
-        });
-    }
+      guild.commands.permissions.set({ fullPermissions });
+    });
+  }
 };
